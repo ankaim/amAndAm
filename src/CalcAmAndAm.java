2c18219bb513a7c7ef359a9446432ae3d4ce9c70
ankaim <kaim@mail.ru>
2017-06-07 18:55:54 +0200

    Написан метод для подсчета среднего арифмитического

--- src/CalcAmAndAm.java
+++ src/CalcAmAndAm.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class CalcAmAndAm {
     public static void main(String[] args) {
-        
+        List<Integer> number = new ArrayList<>();
+        calcAm(number);
     }
+    private static int calcAm(List<Integer> arr){
+        int result=0;
+        for (int i = 0; i <arr.size(); i++) {
+                result+=arr.get(i);
+        }
+        return result/arr.size();
+    }
+
 }
