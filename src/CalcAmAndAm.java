755a9b738f55132ef46c78bc6b44ba7b98f647f5
ankaim <kaim@mail.ru>
2017-06-13 12:41:50 +0200

    Добавлен метод ввода, комментарии и система выхода.

--- src/CalcAmAndAm.java
+++ src/CalcAmAndAm.java
@@ -1,35 +1,69 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 //Два метода для подсчета среднего арифметического и геометрического
 public class CalcAmAndAm {
+    static Scanner sc;
+    static List<Integer> number;
+    private static final String math = "-am";
+    private static final String geom = "-gm";
+
     public static void main(String[] args) {
-        List<Integer> number = new ArrayList<>();
-        calcAm(number);
-        calcGm();
+        number = new ArrayList<>();
+
+        selectMetod();
+
     }
 
-    private static double calcGm() {
-        Scanner x = new Scanner(System.in);
+    private static void selectMetod() {
+        System.out.println("Введите ключ -am, если хотите узнать среднее арифмитическое.\n Если хотите узнать среднее геометрическое, введите -gm");
+        while (true) {
+            sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            if (str.equals(math)) {
+                calcAm();
+                break;
+            }
+            if (str.equals(geom)) {
+                calcGm();
+                break;
+            }
+            System.out.println("Вы ввели не кокректный ключ, попробуйте еще раз.");
+        }
+    }
+
+    private static void calcGm() {
+        System.out.println("Введите числа. Отрицательно число - остановка ввода.");
+        sc = new Scanner(System.in);
         double p = 1, a;
         int c = 0;
-        while (x.hasNext()) {
-            a = x.nextDouble();
-            if (a >= 0) {
-                c++;
-                p *= a;
-            } else break;
+        while (sc.hasNext()) {
+                a = sc.nextDouble();
+                if (a >= 0) {
+                    c++;
+                    p *= a;
+                }else break;
         }
-        //System.out.printf("%.2f", Math.pow(p,1.0/c) );
-        return Math.pow(p, 1.0 / c);
+        System.out.printf("%.2f", Math.pow(p, 1.0 / c));
+        //return Math.pow(p, 1.0 / c);
     }
 
-    private static int calcAm(List<Integer> arr) {
-        int result = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            result += arr.get(i);
+    private static void calcAm() {
+        System.out.println("Введите числа. Пустой ввод - остановка ввода.");
+        sc = new Scanner(System.in);
+        double result = 0;
+        int count = 0;
+        while (true) {
+            String num = sc.nextLine();
+            if (num.equals("")) {
+                break;
+            } else {
+                result += Integer.parseInt(num);
+                count++;
+            }
         }
-        return result / arr.size();
+        System.out.println(result / count);
     }
 
 }
